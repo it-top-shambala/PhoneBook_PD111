c07a1dd655d4e02c6783d0d638194b25f75bd3e6
Starinin Andrey <starininandrey@gmail.com>
2022-07-05 17:46:46 +0300

    Добавление конструкторов в Address.cs и Phone.cs

--- PhoneBook_PD111.Lib/Address.cs
+++ PhoneBook_PD111.Lib/Address.cs
@@ -8,4 +8,18 @@ public class Address
     public string City { get; set; }
     public string Street { get; set; }
     public string Build { get; set; }
+
+    public Address()
+    {
+    }
+
+    public Address(string type, string country, string region, string city, string street, string build)
+    {
+        Type = type;
+        Country = country;
+        Region = region;
+        City = city;
+        Street = street;
+        Build = build;
+    }
 }
--- PhoneBook_PD111.Lib/Phone.cs
+++ PhoneBook_PD111.Lib/Phone.cs
@@ -23,4 +23,14 @@ public class Phone
     
     public string Type { get; set; }
     public string Number { get; set; }
+
+    public Phone()
+    {
+    }
+
+    public Phone(string type, string number)
+    {
+        Type = type;
+        Number = number;
+    }
 }

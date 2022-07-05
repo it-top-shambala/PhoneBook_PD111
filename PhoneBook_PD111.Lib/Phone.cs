b1d5dab0140436496941ddb7dcbfa956f631a86f
Starinin Andrey <starininandrey@gmail.com>
2022-07-05 17:31:25 +0300

    Добавление Phone.cs

---
+++ PhoneBook_PD111.Lib/Phone.cs
@@ -0,0 +1,26 @@
+﻿namespace PhoneBook_PD111.Lib;
+
+public class Phone
+{
+    /*private string _typePhone;
+    public string TypePhone
+    {
+        get => _typePhone;
+        set => _typePhone = value;
+    }*/
+
+    //public string GetTypePhone() => _typePhone;
+    /*public string GetTypePhone()
+    {
+        return _typePhone;
+    }*/
+    
+    //public void SetTypePhone(string type) => _typePhone = type;
+    /*public void SetTypePhone(string type)
+    {
+        _typePhone = type;
+    }*/
+    
+    public string TypePhone { get; set; }
+    public string PhoneNumber { get; set; }
+}

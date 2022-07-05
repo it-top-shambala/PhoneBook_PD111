3e0bd283a28b17e596cbcefc580c1c6aabb7075a
Starinin Andrey <starininandrey@gmail.com>
2022-07-05 17:37:00 +0300

    Добавление Address.cs

---
+++ PhoneBook_PD111.Lib/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace PhoneBook_PD111.Lib;
+
+public class Address
+{
+    public string Type { get; set; }
+    public string Country { get; set; }
+    public string Region { get; set; }
+    public string City { get; set; }
+    public string Street { get; set; }
+    public string Build { get; set; }
+}

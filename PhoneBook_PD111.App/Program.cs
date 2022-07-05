02bc16286ac155de2d02877c70fb88d5e3131aab
Starinin Andrey <starininandrey@gmail.com>
2022-07-05 18:52:58 +0300

    Начало реализации программы

--- PhoneBook_PD111.App/Program.cs
+++ PhoneBook_PD111.App/Program.cs
@@ -1,4 +1,5 @@
-﻿using PhoneBook_PD111.Lib;
+﻿using PhoneBook_PD111.Models;
+using PhoneBook_PD111.Lib.CLI;
 
 namespace PhoneBook_PD111.App;
 
@@ -6,5 +7,41 @@ internal static class Program
 {
     private static void Main()
     {
+        var exit = false;
+        
+        var persons = new List<Person>();
+
+        do
+        {
+            ShowMenu();
+            
+            var select = CLI.Input("Введите номер: ");
+            switch (select)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "0":
+                    exit = true;
+                    break;
+                default:
+                    CLI.ShowError("Неправильный ввод");
+                    break;
+            }
+        } while (!exit);
+        
+        CLI.ShowInfo("До свидания...");
+    }
+
+    private static void ShowMenu()
+    {
+        CLI.ShowInfo("Режимы работы:");
+        CLI.ShowInfo("1. Добавить запись");
+        CLI.ShowInfo("2. Удалить запись");
+        CLI.ShowInfo("3. Показать все записи");
+        CLI.ShowInfo("0. Выйти");
     }
 }

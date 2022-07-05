3242753bf9f83d4331f84d9c1ade0b0517fd7c8a
Starinin Andrey <starininandrey@gmail.com>
2022-07-05 17:47:04 +0300

    Добавление Person.cs

---
+++ PhoneBook_PD111.Lib/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace PhoneBook_PD111.Lib;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public List<string> Groups { get; init; }
+    public List<Phone> Phones { get; init; }
+    public List<Address> Addresses { get; init; }
+
+    public Person(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+
+        Groups = new List<string>();
+        Phones = new List<Phone>();
+        Addresses = new List<Address>();
+    }
+}
